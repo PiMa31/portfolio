406bf3c1fb977451da40a003dff3e5d4ab9a7294
PiMa31 <pierre.masset@bbox.fr>
2021-05-05 14:43:39 +0200

    maj

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => (
         <img className="header__picture"/>
         <h1 className="header__fullname">Pierre MASSET</h1>
         <h2 className="header__job">Développeur Web spécialisé en REACT JS</h2>
-        <p className="header__p">Passionné d'informatique et de jeux vidéos depuis la sortie de la Nintendo 64 (depuis un long moment...), j'ai choisi de m'épanouir dans le milieu du développement </p>
+        <p className="header__p">Passionné d'informatique et de jeux vidéos depuis la sortie de la Nintendo 64 (depuis un long moment...), j'ai choisi de m'épanouir dans le milieu du développement. </p>
         <br></br>
         <p className="header__p">Suite à ma formation à l'école O'Clock et mon passage du titre professionnel, je suis à la recherche de toutes opportunités me permettant d'approfondir mes connaissances et d'évoluer.</p>
         <br></br>
